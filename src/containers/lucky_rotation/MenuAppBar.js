4cd8a62887fde36ce77dd6d08be33a65f0a0dbf4
namhd1992 <stpnghd.vn@gmail.com>
2021-10-14 10:05:54 +0700

    fix

--- src/containers/lucky_rotation/MenuAppBar.js
+++ src/containers/lucky_rotation/MenuAppBar.js
@@ -93,6 +93,7 @@ class MenuAppBar extends React.Component {
 			var code = Ultilities.parse_query_string("code", window.location.href);
 			var fb_mess = Ultilities.parse_query_string("fbmessid", window.location.href);
 			var currentPath=localStorage.getItem("currentPath");
+			
 			if (code != null) {
 				if (fb_mess === null) {
 					var url = Ultilities.base_url() + "user-signin/";
@@ -103,7 +104,7 @@ class MenuAppBar extends React.Component {
 						}
 					}
 					axios.get(url, header).then(function (response) {
-						console.log(response)
+						console.log("AAAAAAAAAAAA:",response)
 						var user_save = response.data;
 						user_save.expired = new Date();
 						localStorage.setItem("user", JSON.stringify(user_save));
@@ -112,6 +113,7 @@ class MenuAppBar extends React.Component {
 						// _this.props.getData(user_save.access_token, user_save.scoinAccessToken).then(function () {
 						// 	window.location.replace(`${window.location.protocol}//${window.location.host}`);
 						// });
+						alert("AAAAAAAA")
 					}).catch(function (error) {
 						_this.props.setStatusServer();
 						localStorage.removeItem("user");
